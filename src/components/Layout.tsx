77c93898df0c00f5f222b72068f00c53c7c9ade6
jorge rios <jorgedrios86@gmail.com>
2026-07-19 20:13:50 -0300

    feat: agregar barra de música fija con Spotify embed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar.tsx';
 import Footer from './Footer.tsx';
+import MusicBar from './MusicBar.tsx';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col bg-bg">
+    <div className="min-h-screen flex flex-col bg-bg pb-24">
       <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
+      <MusicBar />
     </div>
   );
 }
