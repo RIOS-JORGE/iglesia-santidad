48c0f8ed4d72385e0465fb1962059ea8859a7798
jorge rios <jorgedrios86@gmail.com>
2026-07-19 20:53:41 -0300

    feat: eliminar barra de música Spotify completamente

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar.tsx';
 import Footer from './Footer.tsx';
-import MusicBar from './MusicBar.tsx';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col bg-bg pb-24">
+    <div className="min-h-screen flex flex-col bg-bg">
       <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
-      <MusicBar />
     </div>
   );
 }
